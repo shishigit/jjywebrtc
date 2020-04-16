41eb9faf6a02e905c795fa04cebd64d66fd73698
piaoyaocdl <piaoyaocdl@163.com>
2020-04-16 14:23:49 +0800

    1

--- server/WSServer.ts
+++ server/WSServer.ts
@@ -1,6 +1,6 @@
 import WebSocket from "ws";
 
-let connectionArray: { id: string, socket: WebSocket }[] = [];
+let suoyoulianjie: { id: string, socket: WebSocket }[] = [];
 let lianjieid = Date.now();
 
 const WSServer = new WebSocket.Server({port: 6503}, () => console.log('系统启动'));
@@ -8,28 +8,28 @@ const WSServer = new WebSocket.Server({port: 6503}, () => console.log('系统启
 WSServer.on('connection', function (connection: WebSocket)
 {
     // 设定ID
-    connectionArray.push({id: lianjieid.toString(), socket: connection});
+    suoyoulianjie.push({id: lianjieid.toString(), socket: connection});
     connection.send(JSON.stringify({
         type: "id",
         id: lianjieid.toString()
     }));
     lianjieid++;
 
-    console.log('当前连接：', connectionArray.map(value => value.id))
+    console.log('当前连接：', suoyoulianjie.map(value => value.id))
 
     const userListMsgStr = JSON.stringify({
         type: "userlist",
-        users: connectionArray.map(value => value.id)
+        users: suoyoulianjie.map(value => value.id)
     });
 
-    connectionArray.forEach(value => value.socket.send(userListMsgStr))
+    suoyoulianjie.forEach(value => value.socket.send(userListMsgStr))
 
     connection.on('message', function (message: string)
     {
         let msg = JSON.parse(message);
-        let conn = connectionArray.filter(value => value.id === msg.target).pop();
+        let conn = suoyoulianjie.filter(value => value.id === msg.target).pop();
         if (conn) conn.socket.send(message)
     });
 
-    connection.on('close', () => connectionArray = connectionArray.filter(el => el.socket !== connection));
+    connection.on('close', () => suoyoulianjie = suoyoulianjie.filter(el => el.socket !== connection));
 });
