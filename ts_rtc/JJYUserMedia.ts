921aa4994a6699cb1c8d49284fa5109a3902f585
piaoyaocdl <piaoyaocdl@163.com>
2020-04-16 11:09:52 +0800

    1

--- ts_rtc/JJYUserMedia.ts
+++ ts_rtc/JJYUserMedia.ts
@@ -2,6 +2,7 @@ export class JJYUserMedia
 {
     static huoqumeiti(): Promise<MediaStream>
     {
+        // todo 这里需要修改参数
         return navigator.mediaDevices.getUserMedia({video: true, audio: true})
     }
 }
